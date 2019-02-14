420821f0301e5b3e0589ede6fbe621ad4ddb2cd2
sskelkar <skelkar@equalexperts.com>
2019-02-14 14:04:04 +0530

    javadoc text added

--- autofilldb-tester/src/test/java/autofilldbtest/AutoFillForeignKeyColumnsTest.java
+++ autofilldb-tester/src/test/java/autofilldbtest/AutoFillForeignKeyColumnsTest.java
@@ -54,4 +54,24 @@ public class AutoFillForeignKeyColumnsTest extends DBTest {
     //then
     autoFill.into("employee");
   }
+
+  @Test(/* no exception expected */)
+  public void shouldAutomaticallyInsertValueWhenReferencedColumnIsNotTheKeyColumn() {
+    //when
+    runSql(
+      "create table organisation(" +
+        "  id integer," +
+        "  country_code varchar(2) not null unique," +
+        "  primary key(id))");
+
+    runSql(
+      "create table department(" +
+        "  id integer," +
+        "  country_code varchar(2) not null," +
+        "  primary key(id)," +
+        "  foreign key(country_code) references organisation(country_code))");
+
+    autoFill.into("department", of("id", 10));
+    autoFill.into("department", of("id", 20));
+  }
 }
--- autofilldb/src/main/java/autofilldb/AutomaticPopulater.java
+++ autofilldb/src/main/java/autofilldb/AutomaticPopulater.java
@@ -11,11 +11,11 @@ import static java.lang.String.format;
 import static java.util.Collections.emptyMap;
 import static org.apache.logging.log4j.util.Strings.join;
 
-class AutoInsertor {
+class AutomaticPopulater {
   private JdbcTemplate jdbcTemplate;
   private List<DataTypeHandler> dataTypeHandlerHandlers = new ArrayList<>();
 
-  AutoInsertor(JdbcTemplate jdbcTemplate) {
+  AutomaticPopulater(JdbcTemplate jdbcTemplate) {
     this.jdbcTemplate = jdbcTemplate;
     dataTypeHandlerHandlers.add(new IntegerHandler());
     dataTypeHandlerHandlers.add(new VarcharHandler());
@@ -71,7 +71,8 @@ class AutoInsertor {
   }
 
   private List<ColumnDefinition> getColumnDefinitions(String tableName) {
-    String query = String.format("select c.column_name, c.column_type,  c.is_nullable as nullable, c.column_key as column_constraint, c.column_default as default_value, kcu.referenced_table_name as foreign_table, kcu.referenced_column_name as foreign_column\n" +
+    String query = String.format(
+      "select c.column_name, c.column_type,  c.is_nullable as nullable, c.column_key as column_constraint, c.column_default as default_value, kcu.referenced_table_name as foreign_table, kcu.referenced_column_name as foreign_column\n" +
       "from information_schema.columns  c\n" +
       "left join information_schema.key_column_usage kcu on (kcu.column_name = c.column_name and kcu.table_schema=c.table_schema and kcu.table_name=c.table_name)\n" +
       "where c.table_schema=(select database() from dual) and c.table_name='%s'", tableName);
